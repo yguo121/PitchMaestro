447b4b3d646c0b5452b48850760f307e30641481
wyh4788 <yinghuanwang521@gmail.com>
2015-11-18 18:20:44 -0500

    Cleaned up.

--- app/src/main/java/com/example/android/pitchmaestro/HomePageFragment.java
+++ app/src/main/java/com/example/android/pitchmaestro/HomePageFragment.java
@@ -1,10 +1,7 @@
 package com.example.android.pitchmaestro;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

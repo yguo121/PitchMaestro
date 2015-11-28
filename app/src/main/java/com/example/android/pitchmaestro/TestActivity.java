17c58f255b23b5f73b19b47faf89644a752e1219
wyh4788 <yinghuanwang521@gmail.com>
2015-11-27 18:42:30 -0600

    radial menu added. havent figured out how to slide between an activity and a fragment.

---
+++ app/src/main/java/com/example/android/pitchmaestro/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.pitchmaestro;
+
+/**
+ * Created by stephen on 11/27/15.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
